9ccc3545a297af506b1ec3fe63f0962bcc1a1c44
Lucas Feitosa <contato@lucasfeitosa.tech>
2019-07-01 15:50:14 -0300

    Arrumando server.js

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ app.get('/autotreinamento',(req,res)=>{
   for(var i=1;i<4;i++){
     var answer = new Array();
     for(j=1;j<5;j++){
-      
+
       answer.push({title:`Resposta ${i*j}`});
     }
+    var random = Math.floor(Math.random() * 4) + 1  
     var question = {
       title:`Pergunta ${i}`,
-      option:`a`,
+      option:random,
       answer:answer
     };
 
