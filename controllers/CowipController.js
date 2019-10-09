51e9ab2018a00ba203fe8c79ed9289034b5bac44
Lucas Feitosa de Freitas <lucas.freitas@firstdecision.com.br>
2019-10-09 10:43:51 -0300

    Adicionando títull COWIP

--- controllers/CowipController.js
+++ controllers/CowipController.js
@@ -5,6 +5,7 @@ module.exports = {
 
         var coworking1 = {
             idCoworking:1,
+            title:"First Decision",
             address:"Edificio Parque Brasilia, Sala 155 - SIG, Brasilia - DF",
             latitude:"-15.794716",
             longitude:"-47.910953"
@@ -13,6 +14,7 @@ module.exports = {
 
         var coworking2 = {
             idCoworking:2,
+            title:"COWIP CLS 213",
             address:"CLS 213 BL C - Asa Sul, Brasilia - DF",
             latitude:"-15.830894",
             longitude:"-47.915811"
@@ -20,6 +22,7 @@ module.exports = {
 
         var coworking3 = {
             idCoworking:3,
+            title:"COWIP CLS 208",
             address:"SHCS CLS 208 BL B - Asa Sul, Brasília, DF",
             latitude:"-15.819283",
             longitude:"-47.899775"
@@ -28,6 +31,7 @@ module.exports = {
 
         var coworking4 = {
             idCoworking:4,
+            title:"COWIP CLS 203",
             address:"CLS 203 BL D - Asa Sul, Brasilia - DF",
             latitude:"-15.810156",
             longitude:"-47.899172"
@@ -36,6 +40,7 @@ module.exports = {
 
         var coworking5 = {
             idCoworking:5,
+            title:"COWIP CLN 204",
             address:"CLN 204 Bloco B - Asa Norte - Brasília, DF",
             latitude:"-15.776875",
             longitude:"-47.878631"
@@ -44,6 +49,7 @@ module.exports = {
 
         var coworking6 = {
             idCoworking:6,
+            title:"COWIP CLN 406",
             address:"CLN 406 BL E - Asa Norte, Brasília - DF",
             latitude:"-15.766616",
             longitude:"-47.878220"
@@ -52,6 +58,7 @@ module.exports = {
 
         var coworking7 = {
             idCoworking:7,
+            title:"COWIP CLN 410",
             address:"CLN 410 BL C - Asa Norte, Brasília - DF",
             latitude:"-15.753921",
             longitude:"-47.882123"
