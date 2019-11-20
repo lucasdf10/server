dfcc38458f686b1ec9fe01605f47f592e476cc7b
Lucas Feitosa de Freitas <lucas.freitas@firstdecision.com.br>
2019-11-20 15:01:24 -0300

    Adicionando itens para a rota

--- controllers/AlienController.js
+++ controllers/AlienController.js
@@ -9,21 +9,25 @@ module.exports = {
         var stock = {
             idStock:1,
             dsStock:"Consultoria",
+            stockStatus:0,
             epc:"450132674000",
-            items:[
+            itens:[
                 {
                     idItem:1,
+                    itemStatus:0,
                     dsItem:"Monitor",
                     epc:"440132674000",
                 },
                 {
                     idItem:2,
                     dsItem:"Mesa",
+                    itemStatus:0,
                     epc:"440132674001",
                 },
                 {
                     idItem:3,
                     dsItem:"Cadeira",
+                    itemStatus:0,
                     epc:"440132674002",
                 },
             ]
@@ -33,26 +37,89 @@ module.exports = {
         stock = {
             idStock:2,
             dsStock:"Diretoria",
+            stockStatus:1,
             epc:"450132674001",
-            items:[
+            itens:[
                 {
                     idItem:4,
                     dsItem:"Controle",
-                    epc:"440132674004",
+                    itemStatus:0,
+                    epc:"440132674003",
                 },
                 {
                     idItem:5,
                     dsItem:"Ar Condicionado",
-                    epc:"440132674005",
+                    itemStatus:0,
+                    epc:"440132674004",
                 },
                 {
                     idItem:6,
                     dsItem:"Computador",
-                    epc:"440132674006",
+                    itemStatus:0,
+                    epc:"440132674005",
                 },
             ]
         };
         o[key].push(stock);
+
+        stock = {
+            idStock:3,
+            dsStock:"Suporte",
+            stockStatus:2,
+            epc:"450132674002",
+            itens:[
+                {
+                    idItem:7,
+                    dsItem:"Gabinete",
+                    itemStatus:0,
+                    epc:"440132674006",
+                },
+                {
+                    idItem:8,
+                    dsItem:"Alien",
+                    itemStatus:0,
+                    epc:"440132674007",
+                },
+                {
+                    idItem:9,
+                    dsItem:"Carregador MAC",
+                    itemStatus:0,
+                    epc:"440132674008",
+                },
+            ]
+        };
+        o[key].push(stock);
+
+        stock = {
+            idStock:4,
+            dsStock:"Treinamento",
+            stockStatus:3,
+            epc:"450132674003",
+            itens:[
+                {
+                    idItem:10,
+                    dsItem:"Teclado",
+                    itemStatus:0,
+                    epc:"440132674009",
+                },
+                {
+                    idItem:11,
+                    dsItem:"Mouse",
+                    itemStatus:0,
+                    epc:"440132674010",
+                },
+                {
+                    idItem:12,
+                    dsItem:"CPU",
+                    itemStatus:0,
+                    epc:"440132674011",
+                },
+            ]
+        };
+        o[key].push(stock);
+
+
+
         res.json(o);
 
     }
